73641a0729386cb98a4f2b489e08fca734914036
Ahmack-sudo <127269252+Ahmack-sudo@users.noreply.github.com>
2024-06-26 17:10:07 +1200

    Update main_window.rs

--- src/gui/main_window.rs
+++ src/gui/main_window.rs
@@ -931,12 +931,13 @@ pub fn gui_main(recording: bool, input_file: Option<&str>, enable_mpris_cli: boo
             }
             
             Continue(true)
-        }));
+        })
 
         // Don't forget to make all widgets visible.
         
      //   main_window.show_all();
 recognized_song_cover.show();
+        );
         results_frame.hide();
         
         recognize_from_my_speakers_checkbox.hide(); // This will be available only of PulseAudio is up and controllable
